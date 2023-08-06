b9f5bd52fbc2a3cd83bc7089c7aa0bc21586973d
Rafael Guzmán Barranco <guzbarraf@hotmail.com>
2023-08-06 01:28:08 -0600

    efectos visuales para el componente Key

--- src/components/Key/Key.tsx
+++ src/components/Key/Key.tsx
@@ -12,7 +12,7 @@ export const Key = ({
 
   return (
     <button
-      className={`w-[50px] h-[50px] flex items-center justify-center m-2 p-2 rounded-md bg-gray-500 text-8ray-300`}
+      className={`w-[50px] h-[50px] flex items-center justify-center m-2 p-2 rounded-md bg-gray-500 text-8ray-300 hover:scale-110 active:scale-100 duration-200 `}
       onClick={onClick}
     >
       {letter}
