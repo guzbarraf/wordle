0f5ceb00231afbbf4c6289eb2387e0071321cefc
Rafael Guzmán Barranco <guzbarraf@hotmail.com>
2023-08-06 17:45:39 -0600

    validación de si es la tecla de retroceso mostrar la tecla con la imagen

--- src/components/Key/Key.tsx
+++ src/components/Key/Key.tsx
@@ -1,8 +1,16 @@
 "use client";
+import Image from "next/image";
+import imgBack from '@/../public/back.svg';
 
 export interface KeyProps {
   letter: string
   onClick?: () => {} | any;
+}
+
+function ImgBack() {
+  return(
+    <Image src={imgBack} alt={''}/>
+  )
 }
 
 export const Key = ({
@@ -12,10 +20,10 @@ export const Key = ({
 
   return (
     <button
-      className={`w-[50px] h-[50px] flex items-center justify-center m-2 p-2 rounded-md bg-gray-500 text-8ray-300 hover:scale-110 active:scale-100 duration-200 `}
+      className={`min-w-[50px] w-auto h-[50px] m-2 p-2 ${letter !== '<' ? 'pr-4 pl-4' : 'pr-8 pl-8'} flex items-center justify-center rounded-md bg-gray-500 text-8ray-300 hover:scale-110 active:scale-100 duration-200`}
       onClick={onClick}
     >
-      {letter}
+      {letter !== '<' ? letter : <ImgBack/>}
     </button>
   );
 };
