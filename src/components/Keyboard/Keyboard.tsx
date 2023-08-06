380684bf53f4eb832b70283cab0b03fdb4836ded
Rafael Guzmán Barranco <guzbarraf@hotmail.com>
2023-08-06 17:47:29 -0600

    validación dentro del onclick del Key para enviar un caracter o eliminarlo

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -3,50 +3,48 @@ import Key from "@/components/Key";
 
 const letters = [
   [
-    {letter: 'Q', status: ''},
-    {letter: 'W', status: ''},
-    {letter: 'E', status: ''},
-    {letter: 'R', status: ''},
-    {letter: 'T', status: ''},
-    {letter: 'Y', status: ''},
-    {letter: 'U', status: ''},
-    {letter: 'I', status: ''},
-    {letter: 'O', status: ''},
-    {letter: 'P', status: ''},
+    {letter: 'Q', code: 'Q'},
+    {letter: 'W', code: 'W'},
+    {letter: 'E', code: 'E'},
+    {letter: 'R', code: 'R'},
+    {letter: 'T', code: 'T'},
+    {letter: 'Y', code: 'Y'},
+    {letter: 'U', code: 'U'},
+    {letter: 'I', code: 'I'},
+    {letter: 'O', code: 'O'},
+    {letter: 'P', code: 'P'},
   ], [
-    {letter: 'A', status: ''},
-    {letter: 'S', status: ''},
-    {letter: 'D', status: ''},
-    {letter: 'F', status: ''},
-    {letter: 'G', status: ''},
-    {letter: 'H', status: ''},
-    {letter: 'J', status: ''},
-    {letter: 'K', status: ''},
-    {letter: 'L', status: ''},
-    {letter: 'Ñ', status: ''},
+    {letter: 'A', code: 'A'},
+    {letter: 'S', code: 'S'},
+    {letter: 'D', code: 'D'},
+    {letter: 'F', code: 'D'},
+    {letter: 'G', code: 'G'},
+    {letter: 'H', code: 'H'},
+    {letter: 'J', code: 'J'},
+    {letter: 'K', code: 'K'},
+    {letter: 'L', code: 'L'},
+    {letter: 'Ñ', code: 'Ñ'},
   ], [
-    {letter: 'ENTER', status: ''},
-    {letter: 'Z', status: ''},
-    {letter: 'X', status: ''},
-    {letter: 'C', status: ''},
-    {letter: 'V', status: ''},
-    {letter: 'B', status: ''},
-    {letter: 'N', status: ''},
-    {letter: 'M', status: ''},
-    {letter: '<', status: ''},
+    {letter: 'ENTER', code: 'ENTER'},
+    {letter: 'Z', code: 'Z'},
+    {letter: 'X', code: 'X'},
+    {letter: 'C', code: 'C'},
+    {letter: 'V', code: 'V'},
+    {letter: 'B', code: 'B'},
+    {letter: 'N', code: 'N'},
+    {letter: 'M', code: 'M'},
+    {letter: '<', code: '<'},
   ],
 ];
 
 export interface KeyboardProps {
-  setKeyPressed: any;
+  fnKeyPressed: (key: string) => void;
+  fnDeletedLetter: () => void;
 }
 
-export const Keyboard = ({
-  setKeyPressed,
-}: KeyboardProps) => {
+export const Keyboard = (x: KeyboardProps) => {
 
   return (
-    // <div className={`container grid grid-rows-3 grid-cols-10 gap-0 max-w-[650px] w-auto h-auto m-2 p-2 rounded-md bg-gray-300 `}>
     <div className={`flex items-center justify-center flex-col max-w-[700px] w-auto h-auto m-4 p-2 rounded-md bg-gray-300 `}>
       {letters.map((letter: any, indexP: number) => {
         // console.log('letter =>', letter);
@@ -60,7 +58,13 @@ export const Keyboard = ({
                   letter={item.letter}
                   onClick={() => {
                     // console.log('Click =>', item.letter);
-                    setKeyPressed(item.letter);
+                    if (item.letter !== 'ENTER') {
+                      if (item.letter === '<') {
+                        x.fnDeletedLetter();
+                      } else {
+                        x.fnKeyPressed(item.letter);
+                      }
+                    }
                   }}
                 />
               )
