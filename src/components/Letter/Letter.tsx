65296773da6d40694647a7e2d17ec6ea8d09f989
Rafael Guzmán Barranco <guzbarraf@hotmail.com>
2023-08-05 22:53:04 -0600

    componente para las letras del grid

---
+++ src/components/Letter/Letter.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+export interface LetterProps {
+  id: string;
+  letter: string;
+  bgColor: string;
+  index: number;
+  validate: boolean;
+  x: number;
+  y: number;
+}
+
+export const Letter = ({
+  id,
+  letter,
+  bgColor,
+  validate,
+}: LetterProps) => {
+  
+  return (
+    <div
+      id={id}
+      className={`
+        w-[72px] h-[72px] flex items-center justify-center m-2 p-2 rounded-md
+        ${validate ? bgColor : 'bg-gray-400'}
+         font-bold
+      `}>
+      {letter}
+    </div>
+  );
+};
