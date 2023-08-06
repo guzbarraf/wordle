8030c8bfc3b17765ccdea641caf4584e116383a1
Rafael Guzmán Barranco <guzbarraf@hotmail.com>
2023-08-06 01:32:04 -0600

    implementación de componentes de la página principal del juego

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,38 @@
-import ThemeSwitcher from "@/components/ThemeSwitcher";
+'use client'
+import GridLetters from "@/components/GridLetters";
+import {useGame} from "@/utils/useGame";
+import Keyboard from "@/components/Keyboard";
+import {Header} from "@/components/Header/Header";
+import HowPlay from "@/components/HowPlay";
 
 export default function Home() {
+  const {
+    showHowPlay,
+    setShowHowPlay,
+    word,
+    originalWord,
+    guesses,
+    validateRow,
+    setKeyPressed,
+    gameOver,
+  } = useGame(1);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-50 dark:bg-slate-900">
-      <div>
-        <ThemeSwitcher/>
-      </div>
-      <div className={'text-black dark:invert'}>QWERTY</div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 min-w-full max-w-5xl bg-slate-50 dark:bg-slate-900">
+      <Header/>
+
+      <div className={'text-black dark:invert'}>{word}</div>
+
+      <GridLetters guesses={guesses} validateRow={validateRow}/>
+
+      <Keyboard setKeyPressed={setKeyPressed}/>
+
+      {showHowPlay ? <HowPlay setShowHowPlay={setShowHowPlay}/> : null}
+
+      {gameOver === 'win' ? (
+        <div className={'text-black dark:invert'}>WIN!!!</div>
+      ) : null}
+
     </main>
   )
 }
-
-
-
